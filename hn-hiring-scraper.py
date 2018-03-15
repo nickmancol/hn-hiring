ff8d3d8e801989cad2ec91c3d705b16c3c1ba6e7
Nicolas Bohorquez <nicolas.bohorquez@alephsa.com>
2018-03-15 11:41:51 +0100

    Added basic html client support, basic test data, js dependencies

--- hn-hiring-scraper.py
+++ hn-hiring-scraper.py
@@ -1,18 +1,22 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-import json
+import csv
 
 # specify the url
-src_pages = ['16492994', '16282819', '16052538']
+src_pages = ['16052538', '16492994', '16282819' ]
 
 def extract_header(text=''):
     header = '|'.join(p[0:min(50, len(p))] for p in text.replace('\n', ' ').split('|'))
     return header
 
-def extract_opps(src_pages=[]):
-    opps = []
+def extract_opps(src_pages=[], local=False):
+    opps = [('article_id', 'header', 'text')]
+    src_pages.sort(reverse=True)
+
     for pageid in src_pages:
-        src_page = 'https://news.ycombinator.com/item?id='+pageid
+        src_page = pageid
+        if not local:
+            src_page = 'https://news.ycombinator.com/item?id='+pageid
         # query the website and return the html to the variable 'page'
         page = urlopen(src_page)
 
@@ -25,18 +29,23 @@ def extract_opps(src_pages=[]):
             comm_id = o.get('id')
             a = o.find('span', attrs={'class': 'c00'})
             if a:
-                text = a.text.strip()
+                text = a.text.strip().replace('\nreply','')
                 #TODO: change this selection criteria
                 if '|' in text:
                     header = extract_header(text)
-                    opps.append({'id': comm_id, 'text': text, 'header': header})
+                    opps.append((comm_id, header, text))
 
     return opps
 
 
 def serialize(filename, opps=[]):
-    with open(filename, 'w') as outfile:
-        json.dump(opps, outfile)
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(opps)
 
 if __name__ != "main":
-    serialize('data.json', extract_opps(src_pages))
+    serialize('data.csv', extract_opps(
+        ['file:///home/nickman/code/hn-hiring/data/1-2018.html'
+        ,'file:///home/nickman/code/hn-hiring/data/2-2018.html'
+        ,'file:///home/nickman/code/hn-hiring/data/3-2018.html']
+            , local=True))
